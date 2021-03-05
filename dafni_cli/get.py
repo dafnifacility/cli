7c3e232b2dbad1335e66ba55f6cdd9b976f94eb3
Ian Roper <ian.roper@tessella.com>
2021-03-05 17:42:56 +0000

    Refactored model.py and get.py so a Model class is now created for people to use.

--- dafni_cli/get.py
+++ dafni_cli/get.py
@@ -1,7 +1,8 @@
 import click
 
 from dafni_cli.login import login
-import dafni_cli.model as model
+from dafni_cli.API_requests import get_models_dicts
+from dafni_cli.model import create_model_list
 
 
 @click.group()
@@ -15,27 +16,36 @@ def get(ctx):
 
 
 @get.command(help="List and filter models")
-@click.option('--summary/--no-summary', default=False,
-              help="Display short summary of model")
-@click.option('--descr/--no-descr', default=False,
-              help="Display full description of model")
 @click.option('--creation-date', default=None,
               help="Filter for models created since given date. Format: DD/MM/YYYY")
 @click.option('--publication-date', default=None,
               help="Filter for models published since given date. Format: DD/MM/YYYY")
 @click.pass_context
-def models(ctx, summary, descr, creation_date, publication_date):
+def models(ctx, creation_date, publication_date):
     """Displays list of model names with other options allowing
         more details to be listed as well.
 
         Args:
             ctx (context): contains JWT for authentication
-            summary (bool): whether summary should be displayed
-            descr (bool): whether description should be displayed
             creation_date (str): for filtering by creation date. Format: DD/MM/YYYY
             publication_date (str): for filtering by publication date. Format: DD/MM/YYYY
         """
-    model.list_groups(ctx, summary, descr, creation_date, publication_date)
+    model_dict_list = get_models_dicts(ctx.obj['jwt'])
+    model_list = create_model_list(model_dict_list)
+    for model in model_list:
+        date_filter = True
+        if creation_date:
+            date_filter = model.filter_by_date("creation", creation_date)
+        if publication_date:
+            date_filter = model.filter_by_date("publication", publication_date)
+        if date_filter:
+            model.output_model_details()
+
+
+@get.command()
+@click.pass_context
+def metadata(ctx):
+    pass
 
 
 @get.command()
--- dafni_cli/model.py
+++ dafni_cli/model.py
@@ -1,72 +1,94 @@
 import click
-import requests
 import datetime as dt
 from dateutil import parser
 
-from dafni_cli.urls import MODELS_API_URL
+from dafni_cli.API_requests import get_models_dicts, get_single_model_dict, get_model_metadata_dicts
+from dafni_cli.login import DATE_TIME_FORMAT, login
 
 
-def get_models_dicts(jwt_dict: dict) -> list:
-    """Function to call the list models endpoint and return the resulting list of dictionaries.
+class Model:
+    def __init__(self, identifier=None):
+        self.version_id = identifier
+        self.display_name = None
+        self.summary = None
+        self.description = None
+        self.creation_time = None
+        self.publication_time = None
+        self.version_tags = None
+        self.container = None
+        self.name = None
+        self.inputs = None
+        self.outputs = None
+        pass
 
-    Args:
-        jwt_dict (dict): JWT dictionary
+    def get_details_from_dict(self, model_dict: dict):
+        self.display_name = model_dict['name']
+        self.summary = model_dict['summary']
+        self.description = model_dict['description']
+        self.creation_time = parser.isoparse(model_dict['creation_date'])
+        self.publication_time = parser.isoparse(model_dict['publication_date'])
+        self.version_id = model_dict['id']
+        self.version_tags = model_dict['version_tags']
+        self.container = model_dict['container']
 
-    Returns:
-        list: list of dictionaries with raw response from API
-    """
-    models_request = requests.get(
-        MODELS_API_URL + '/models/',
-        headers={
-            "Content-Type": "application/json",
-            "authorization": jwt_dict
-        },
-        allow_redirects=False
-    )
-    models_request.raise_for_status()
-    return models_request.json()
+    def get_details_from_id(self, jwt_string: str, version_id_string: str):
+        model_dict = get_single_model_dict(jwt_string, version_id_string)
+        self.get_details_from_dict(model_dict)
+
+    def get_metadata(self, jwt_string: str):
+        metadata_dict = get_model_metadata_dicts(jwt_string, self.version_id)
+        self.name = metadata_dict['metadata']['name']
+        self.inputs = metadata_dict['spec']['inputs']
+        self.outputs = metadata_dict['spec']['outputs']
+
+    def filter_by_date(self, key: str, date: str) -> bool:
+        """Filters models based on the date given as an option.
+            Args:
+                key (str): Key for MODEL_DICT in which date is contained
+                date (str): Date for which models are to be filtered on: format DD/MM/YYYY
+
+            Returns:
+                bool: Whether to display the model based on the filter
+        """
+        day, month, year = date.split("/")
+        date = dt.date(int(year), int(month), int(day))
+        if key == "creation":
+            return self.creation_time.date() >= date
+        elif key == "publication":
+            return self.publication_time.date() >= date
+        else:
+            raise Exception("Key should be CREATION or PUBLICATION")
+
+    def output_model_details(self):
+        """Prints relevant model attributes to command line"""
+        click.echo("Name: " + self.display_name +
+                   "     ID: " + self.version_id +
+                   "     Date: " + self.creation_time.date().strftime(DATE_TIME_FORMAT))
+        click.echo("Summary: " + self.summary)
+
+    def output_model_metadata(self, ctx, model_version_id: str):
+        """Displays the metadata for the model.
+
+        Args:
+            ctx (context): contains JWT for authentication
+            model_version_id (str): Version ID for the model to display the metadata of
+        """
+        # TODO Implement this
+        pass
 
 
-def filter_by_date(model_dict: dict, key: str, date: str) -> bool:
-    """Filters models based on the date given as an option.
-    Args:
-        model_dict (dict): Dictionary returned by API for a single model
-        key (str): Key for MODEL_DICT in which date is contained
-        date (str): Date for which models are to be filtered on
-
-    Returns:
-        bool: Whether to display the model based on the filter
-    """
-    day,month,year = date.split("/")
-    date = dt.date(int(year), int(month), int(day))
-    iso_date = model_dict[key]
-    model_date = parser.isoparse(iso_date).date()
-    return model_date >= date
+def create_model_list(model_dict_list: list) -> list:
+    model_list = []
+    for model_dict in model_dict_list:
+        model = Model()
+        model.get_details_from_dict(model_dict)
+        model_list.append(model)
+    return model_list
 
 
-def list_groups(ctx, summary, descr, creation_date, publication_date):
-    """Lists requested information about non-filtered models.
-    Args:
-        ctx (context): contains JWT for authentication
-        summary (bool): whether summary should be displayed
-        descr (bool): whether description should be displayed
-        creation_date (str): for filtering by creation date. Format: DD/MM/YYYY
-        publication_date (str): for filtering by publication date. Format: DD/MM/YYYY
-    """
-
-    models_dicts = get_models_dicts(ctx.obj['jwt'])
-    for model_dict in models_dicts:
-        date_filter = True
-        if creation_date:
-            date_filter = filter_by_date(model_dict, "creation_date", creation_date)
-        if publication_date:
-            date_filter = filter_by_date(model_dict, "publication_date", publication_date)
-        if date_filter:
-            if summary or descr:
-                click.echo("Name: " + model_dict['name'])
-            else:
-                click.echo(model_dict['name'])
-            if summary:
-                click.echo("Summary: " + model_dict['summary'] + "\n")
-            if descr:
-                click.echo("Description: \n" + model_dict['description'] + "\n")
+if __name__ == "__main__":
+    jwt = "JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJsb2dpbi1hcHAtand0IiwiZXhwIjoxNjE0OTY3ODQ4LCJzdWIiOiI4ZDg1N2FjZi0yNjRmLTQ5Y2QtOWU3Zi0xZTlmZmQzY2U2N2EifQ.UxB_67FpglFaJR2pBlVw_mIJY2zQ5y6cNEG5ZRmW8WA"
+    version_id = "0b4b0d0a-5b05-4e14-b382-9a5c9082315b"
+    model = Model()
+    model.get_details_from_id(jwt, version_id)
+    model.output_model_details()
