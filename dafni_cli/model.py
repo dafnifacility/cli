84eb682b39f3b61732dadecc871b0833a3aa03db
Ian Roper <ian.roper@tessella.com>
2021-03-09 12:47:24 +0000

    Finished formatting metadata file output.

--- dafni_cli/model.py
+++ dafni_cli/model.py
@@ -3,6 +3,7 @@ import datetime as dt
 from dateutil import parser
 import json
 import textwrap
+from typing import Optional
 
 from dafni_cli.API_requests import get_models_dicts, get_single_model_dict, get_model_metadata_dicts
 from dafni_cli.login import DATE_TIME_FORMAT, login
@@ -15,7 +16,36 @@ from dafni_cli.consts import INPUT_TITLE_HEADER, \
     INPUT_DESCRIPTION_HEADER, \
     INPUT_TYPE_COLUMN_WIDTH, \
     INPUT_MIN_MAX_COLUMN_WIDTH, \
-    INPUT_DESCRIPTION_LINE_WIDTH
+    INPUT_DESCRIPTION_LINE_WIDTH, \
+    OUTPUT_NAME_HEADER, \
+    OUTPUT_FORMAT_HEADER, \
+    OUTPUT_SUMMARY_HEADER, \
+    OUTPUT_FORMAT_COLUMN_WIDTH, \
+    OUTPUT_SUMMARY_COLUMN_WIDTH, \
+    CONSOLE_WIDTH
+
+
+def optional_column(dictionary: dict, key: str, column_width: int = 0, alignment: str = "<"):
+    """Adds a value to a column, if the key exists in the dictionary
+    and adds spaces of the appropriate width if not.
+    Args:
+         dictionary (dict): Dictionary with data inside
+         key (str): Key of the data that is to be checked and added if present
+         column_width (int): Number of spaces to be returned instead if the key is not present
+         alignment (str): Specified alignment of column
+    Returns:
+        entry (str): Either the value of the entry to be put into the table, column_width number of spaces
+    """
+    if key in dictionary:
+        if column_width > 0:
+            entry = f"{dictionary[key]:{alignment}{column_width}}"
+        elif column_width == 0:
+            entry = f"{dictionary[key]}"
+        else:
+            raise Exception("Column width for optional column must be positive or zero")
+    else:
+        entry = f" " * column_width
+    return entry
 
 
 class Model:
@@ -51,11 +81,12 @@ class Model:
 
     def get_metadata(self, jwt_string: str):
         metadata_dict = get_model_metadata_dicts(jwt_string, self.version_id)
-        click.echo(json.dumps(metadata_dict, indent=2))
         self.name = metadata_dict['metadata']['name']
         self.owner = metadata_dict['metadata']['owner']
-        self.inputs = metadata_dict['spec']['inputs']
-        self.outputs = metadata_dict['spec']['outputs']
+        if 'inputs' in metadata_dict['spec']:
+            self.inputs = metadata_dict['spec']['inputs']
+        if 'outputs' in metadata_dict['spec']:
+            self.outputs = metadata_dict['spec']['outputs']
 
     def filter_by_date(self, key: str, date: str) -> bool:
         """Filters models based on the date given as an option.
@@ -75,35 +106,74 @@ class Model:
         else:
             raise Exception("Key should be CREATION or PUBLICATION")
 
-    def output_inputs(self) -> str:
-        # Parameters
+    def format_parameters(self) -> str:
+        """Formats input parameters for the model into a string which prints as a table"""
         parameters = self.inputs['env']
         titles = [parameter['title'] for parameter in parameters] + ["title"]
-        defaults = [str(parameter['default']) for parameter in parameters] + ["default"]
+        defaults = ["default"]
+        for parameter in parameters:
+            if 'default' in parameter:
+                defaults.append('default')
         max_title_length = len(max(titles, key=len)) + 2
         max_default_length = len(max(defaults, key=len)) + 2
-        inputs = f"{INPUT_TITLE_HEADER:{max_title_length}}" \
-                 f" {INPUT_TYPE_HEADER:{INPUT_TYPE_COLUMN_WIDTH}}" \
-                 f" {INPUT_MIN_HEADER:{INPUT_MIN_MAX_COLUMN_WIDTH}}" \
-                 f" {INPUT_MAX_HEADER:{INPUT_MIN_MAX_COLUMN_WIDTH}}" \
-                 f" {INPUT_DEFAULT_HEADER:{max_default_length}}" \
-                 f" {INPUT_DESCRIPTION_HEADER}\n" \
-                 + f"-" * (max_title_length +
-                           INPUT_TYPE_COLUMN_WIDTH +
-                           2 * INPUT_MIN_MAX_COLUMN_WIDTH +
-                           max_default_length +
-                           INPUT_DESCRIPTION_LINE_WIDTH) \
-                 + "\n"
+        # Setup headers
+        params_table = f"{INPUT_TITLE_HEADER:{max_title_length}}" \
+                       f"{INPUT_TYPE_HEADER:{INPUT_TYPE_COLUMN_WIDTH}}" \
+                       f"{INPUT_MIN_HEADER:{INPUT_MIN_MAX_COLUMN_WIDTH}}" \
+                       f"{INPUT_MAX_HEADER:{INPUT_MIN_MAX_COLUMN_WIDTH}}" \
+                       f"{INPUT_DEFAULT_HEADER:{max_default_length}}" \
+                       f"{INPUT_DESCRIPTION_HEADER}\n" \
+                       + f"-" * (max_title_length +
+                                 INPUT_TYPE_COLUMN_WIDTH +
+                                 2 * INPUT_MIN_MAX_COLUMN_WIDTH +
+                                 max_default_length +
+                                 INPUT_DESCRIPTION_LINE_WIDTH) \
+                       + "\n"
+        # Populate table
         for parameter in parameters:
-            inputs += f"{parameter['title']:{max_title_length}} {parameter['type']:{INPUT_TYPE_COLUMN_WIDTH}} "
-            if "min" in parameter and "max" in parameter:
-                inputs += f"{parameter['min']:<{INPUT_MIN_MAX_COLUMN_WIDTH}} " \
-                          f"{parameter['max']:<{INPUT_MIN_MAX_COLUMN_WIDTH}} "
-            else:
-                inputs += f" " * (INPUT_MIN_MAX_COLUMN_WIDTH + 1) * 2
-            inputs += f"{parameter['default']:<{max_default_length}}" \
-                      f"{parameter['desc']}\n"
-        return inputs
+            params_table += f"{parameter['title']:{max_title_length}}{parameter['type']:{INPUT_TYPE_COLUMN_WIDTH}}"
+            params_table += optional_column(parameter, "min", INPUT_MIN_MAX_COLUMN_WIDTH)
+            params_table += optional_column(parameter, "max", INPUT_MIN_MAX_COLUMN_WIDTH)
+            params_table += optional_column(parameter, "default", max_default_length)
+            params_table += f"{parameter['desc']}\n"
+        return params_table
+
+    def format_dataslots(self) -> Optional[str]:
+        """Formats input data slots to print in a clear way"""
+        if 'dataslots' in self.inputs:
+            dataslots = self.inputs['dataslots']
+            dataslots_list = ''
+            for dataslot in dataslots:
+                dataslots_list += 'Name: ' + dataslot['name'] + '\n'
+                dataslots_list += 'Path in container: ' + dataslot['path'] + '\n'
+                dataslots_list += 'Required: {}\n'.format(dataslot['required'])
+                dataslots_list += 'Default Datasets: \n'
+                for default in dataslot['default']:
+                    # TODO print name using API call to databases
+                    dataslots_list += 'Name: '
+                    dataslots_list += f'ID: {default["uid"]}     '
+                    dataslots_list += f'Version ID: {default["uid"]}     '
+                dataslots_list += '\n'
+            return dataslots_list
+        else:
+            return None
+
+    def format_outputs(self) -> str:
+        """Formats output files into a string which prints as a table"""
+        names = [output['name'] for output in self.outputs['datasets']] + ["Name"]
+        max_name_length = len(max(names, key=len)) + 2
+        outputs_table = f"{OUTPUT_NAME_HEADER:{max_name_length}}" \
+                        f"{OUTPUT_FORMAT_HEADER:{OUTPUT_FORMAT_COLUMN_WIDTH}}" \
+                        f"{OUTPUT_SUMMARY_HEADER}\n" \
+                        + f"-" * (max_name_length
+                                  + OUTPUT_FORMAT_COLUMN_WIDTH
+                                  + OUTPUT_SUMMARY_COLUMN_WIDTH) \
+                        + "\n"
+        for dataset in self.outputs['datasets']:
+            outputs_table += f"{dataset['name']:{max_name_length}}{dataset['type']:{OUTPUT_FORMAT_COLUMN_WIDTH}}"
+            outputs_table += optional_column(dataset, "description")
+            outputs_table += '\n'
+        return outputs_table
 
     def output_model_details(self):
         """Prints relevant model attributes to command line"""
@@ -117,15 +187,20 @@ class Model:
         click.echo("Name: " + self.display_name)
         click.echo("Date: " + self.creation_time.strftime('%B %d %Y'))
         click.echo("Summary: ")
-        click.echo(self.summary + "\n")
+        click.echo(self.summary)
         click.echo("Description: ")
         for paragraph in self.description.split("\n"):
-            for line in textwrap.wrap(paragraph, width=120):
+            for line in textwrap.wrap(paragraph, width=CONSOLE_WIDTH):
                 click.echo(line)
-        click.echo("\nInputs: ")
-        click.echo(self.output_inputs())
-        click.echo("Outputs: ")
-        click.echo(self.outputs)
+        click.echo("")
+        if self.inputs:
+            click.echo("Input Parameters: ")
+            click.echo(self.format_parameters())
+            click.echo("Input Data Slots: ")
+            click.echo(self.format_dataslots())
+        if self.outputs:
+            click.echo("Outputs: ")
+            click.echo(self.format_outputs())
         pass
 
 
@@ -136,12 +211,3 @@ def create_model_list(model_dict_list: list) -> list:
         single_model.get_details_from_dict(model_dict)
         model_list.append(single_model)
     return model_list
-
-
-if __name__ == "__main__":
-    jwt = "JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJsb2dpbi1hcHAtand0IiwiZXhwIjoxNjE1MjE3NjE3LCJzdWIiOiI4ZDg1N2FjZi0yNjRmLTQ5Y2QtOWU3Zi0xZTlmZmQzY2U2N2EifQ.gaiPaXQ8SkHFQwen4ujMQD1pbJEY9Md--cd1V1mai9A"
-    version_id = "9de4ad50-fd98-4def-9bfc-39378854e6a1"
-    model = Model()
-    model.get_details_from_id(jwt, version_id)
-    model.get_metadata(jwt)
-    model.output_model_metadata()
