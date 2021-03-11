33e3c2f2b6a85f80222c5769a391eaa7fc4aba6c
jamie-short <jamie.short@tessella.com>
2021-03-11 13:17:44 +0000

    restructring api folder

---
+++ dafni_cli/api/dafni_api.py
@@ -0,0 +1,40 @@
+import requests
+from typing import Union, List
+
+from dafni_cli.consts import MODELS_API_URL, DISCOVERY_API_URL
+
+
+def dafni_get_request(
+    url: str, jwt: str, allow_redirect: bool = False
+) -> Union[list, dict]:
+    """Performs a GET request from the DAFNI API.
+    If a status other than 200 is returned, an exception will be raised.
+
+    Args:
+        url (str): The url endpoint that is being queried
+        jwt (str): JWT
+        allow_redirect (bool): Flag to allow redirects during API call. Defaults to False.
+
+    Returns:
+        list: For an endpoint returning several objects, a list is returned (e.g. /models/).
+        dict: For an endpoint returning one object, this will be a dictionary (e.g. /models/<version_id>).
+    """
+    response = requests.get(
+        url,
+        headers={"Content-Type": "application/json", "authorization": jwt},
+        allow_redirects=allow_redirect,
+    )
+    response.raise_for_status()
+    return response.json()
+
+
+def dafni_post_request(url: str, jwt: str, data: dict, allow_redirect: bool = False):
+
+    response = requests.post(
+        url,
+        headers={"Content-Type": "application/json", "authorization": jwt},
+        allow_redirects=allow_redirect,
+        json=data,
+    )
+    response.raise_for_status()
+    return response.json()
---
+++ dafni_cli/api/datasets_api.py
@@ -0,0 +1,21 @@
+import requests
+from typing import Union, List
+
+from dafni_cli.consts import MODELS_API_URL, DISCOVERY_API_URL
+from dafni_cli.api.dafni_api import dafni_get_request, dafni_post_request
+
+
+def get_all_datasets(jwt: str) -> List[dict]:
+    """Function to retrieve all datasets available to the
+    given user
+
+    Args:
+        jwt (str): Users JWT
+
+    Returns:
+        List[dict]: List of available datasets
+    """
+    url = DISCOVERY_API_URL + "/catalogue/"
+    data = {"sort_by": "recent"}
+
+    return dafni_post_request(url, jwt, data)
