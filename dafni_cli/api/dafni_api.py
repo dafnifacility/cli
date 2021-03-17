9ccbaa532b902cb70cbbbc3057143bfffa8516a1
Ian Roper <ian.roper@tessella.com>
2021-03-17 13:15:19 +0000

    Added put requests and more model api requests for upload.

--- dafni_cli/api/dafni_api.py
+++ dafni_cli/api/dafni_api.py
@@ -1,12 +1,12 @@
 import requests
-from typing import Union, List
+from typing import Union, List, Optional
 
 from dafni_cli.consts import MODELS_API_URL, DISCOVERY_API_URL
 
 
 def dafni_get_request(
     url: str, jwt: str, allow_redirect: bool = False
-) -> Union[list, dict]:
+) -> Union[List[dict], dict]:
     """Performs a GET request from the DAFNI API.
     If a status other than 200 is returned, an exception will be raised.
 
@@ -16,7 +16,7 @@ def dafni_get_request(
         allow_redirect (bool): Flag to allow redirects during API call. Defaults to False.
 
     Returns:
-        list: For an endpoint returning several objects, a list is returned (e.g. /models/).
+        List[dict]: For an endpoint returning several objects, a list is returned (e.g. /models/).
         dict: For an endpoint returning one object, this will be a dictionary (e.g. /models/<version_id>).
     """
     response = requests.get(
@@ -30,7 +30,7 @@ def dafni_get_request(
 
 def dafni_post_request(
     url: str, jwt: str, data: dict, allow_redirect: bool = False
-) -> List[dict]:
+) -> Union[List[dict], dict]:
     """Performs a POST request from the DAFNI API.
     If a status other than 200 is returned, an exception will be raised.
 
@@ -42,12 +42,37 @@ def dafni_post_request(
 
     Returns:
         List[dict]: For an endpoint returning several objects, a list is returned (e.g. /catalogue/)
+        dict: For an endpoint requesting upload urls (e.g. /models/upload/)
     """
     response = requests.post(
         url,
         headers={"Content-Type": "application/json", "authorization": jwt},
         allow_redirects=allow_redirect,
         json=data,
+    )
+    response.raise_for_status()
+    return response.json()
+
+
+def dafni_put_request(
+    url: str, jwt: str, data, content_type: str
+) -> Optional[bool]:
+    """Performs a PUT request from the DAFNI API.
+    If a status other than 200 is returned, an exception will be raised.
+
+    Args:
+        url (str): The url endpoint that is being queried
+        jwt (str): JWT
+        data: The data to be PUT-ed
+        content_type (str): Content type for the headers
+
+    Returns:
+        bool: Response when validating data
+    """
+    response = requests.post(
+        url,
+        headers={"Content-Type": content_type, "authorization": jwt},
+        data=data,
     )
     response.raise_for_status()
     return response.json()
--- dafni_cli/api/models_api.py
+++ dafni_cli/api/models_api.py
@@ -1,8 +1,13 @@
 import requests
-from typing import Union, List
+from typing import Union, List, Tuple, Optional
+from pathlib import Path
 
 from dafni_cli.consts import MODELS_API_URL, DISCOVERY_API_URL
-from dafni_cli.api.dafni_api import dafni_get_request
+from dafni_cli.api.dafni_api import (
+    dafni_get_request,
+    dafni_post_request,
+    dafni_put_request
+)
 
 
 def get_models_dicts(jwt: str) -> List[dict]:
@@ -25,8 +30,8 @@ def get_single_model_dict(jwt: str, model_version_id: str) -> dict:
         jwt (str): JWT
         model_version_id (str): model version ID for selected model
 
-        Returns:
-            dict: dictionary for the details of selected model
+    Returns:
+        dict: dictionary for the details of selected model
     """
     url = MODELS_API_URL + "/models/" + model_version_id + "/"
     return dafni_get_request(url, jwt)
@@ -39,8 +44,92 @@ def get_model_metadata_dict(jwt: str, model_version_id: str) -> dict:
         jwt (str): JWT
         model_version_id (str): model version ID for selected model
 
-        Returns:
-            dict: dictionary for the metadata of selected model
+    Returns:
+        dict: dictionary for the metadata of selected model
     """
     url = MODELS_API_URL + "/models/" + model_version_id + "/definition/"
     return dafni_get_request(url, jwt)
+
+
+def validate_model_definition(
+    jwt: str, model_definition: Path
+) -> Optional[bool]:
+    """Validates the model definition file using a PUT to the DAFNI API
+
+    Args:
+        jwt (str): JWT
+        model_definition (Path): Path to the model definition file
+
+    Returns:
+        bool: Whether the model definition is valid or not
+    """
+    content_type = "application/yaml"
+    url = MODELS_API_URL + "/models/definition/validate/"
+    with model_definition.open("rb") as md:
+        return dafni_put_request(url, jwt, md, content_type)
+
+
+def get_model_upload_urls(
+    jwt: str
+) -> Tuple[str, dict]:
+    """Obtains the model upload urls from the DAFNI API
+
+    Args:
+        jwt (str): JWT
+
+    Returns:
+        str: The ID for the upload
+        dict: The urls for the definition and image with keys "definition" and "image", respectively.
+    """
+    url = MODELS_API_URL + "/models/upload/"
+    data = {"image": True, "definition": True}
+    urls_resp = dafni_post_request(url, jwt, data)
+    upload_id = urls_resp["id"]
+    urls = urls_resp["urls"]
+    return upload_id, urls
+
+
+def upload_file_to_minio(
+    jwt: str, url: str, file_path: Path
+) -> None:
+    """Function to upload definition or image files to DAFNI
+
+    Args:
+        jwt (str): JWT
+        url (str): URL for the file
+        file_path (Path): Path to the file
+    """
+    content_type = "multipart/form-data"
+    with file_path.open("rb") as file_data:
+        dafni_put_request(url, jwt, file_data, content_type)
+
+
+def model_ingest(
+    jwt: str, upload_id: str, version_message: str
+) -> None:
+    """Ingests a new model to DAFNI
+
+    Args:
+        jwt (str): JWT
+        upload_id (str): Upload ID
+        version_message (str): Message to be attached to this version
+    """
+    url = MODELS_API_URL + "/models/upload/" + upload_id + "/ingest/"
+    data = {"version_message": version_message}
+    response = dafni_post_request(url, jwt, data)
+
+
+def model_version_ingest(
+    jwt: str, upload_id: str, model_id: str, version_message: str
+) -> None:
+    """Ingests a new version of a model to DAFNI
+
+        Args:
+            jwt (str): JWT
+            upload_id (str): Upload ID
+            model_id (str): ID of existing parent model
+            version_message (str): Message to be attached to this version
+    """
+    url = MODELS_API_URL + "/models/" + model_id + "/upload/" + upload_id + "/ingest/"
+    data = {"version_message": version_message}
+    response = dafni_post_request(url, jwt, data)
