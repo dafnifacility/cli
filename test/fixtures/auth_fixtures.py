bcc15af4e8d02546c0894330b1a915d5e335ba61
Ian Roper <ian.roper@tessella.com>
2021-03-26 11:08:14 +0000

    Added docstrings for mock auth dictionaries and added commands/__init__.py.

--- test/fixtures/auth_fixtures.py
+++ test/fixtures/auth_fixtures.py
@@ -3,6 +3,12 @@ import pytest
 
 @pytest.fixture
 def model_auth_fixture() -> dict:
+    """Function to generate an auth dictionary with identical keys to those
+    returned from a get model request.
+
+    Returns:
+        dict: Mock auth dictionary
+    """
     auth = {
       "asset_id": "0a0a0a0a-0a00-0a00-a000-0a0a0000000a",
       "reason": "reason for access",
@@ -16,6 +22,12 @@ def model_auth_fixture() -> dict:
 
 @pytest.fixture
 def dataset_auth_fixture() -> dict:
+    """Function to generate an auth dictionary with identical keys to those
+        returned from a get dataset request.
+
+    Returns:
+        dict: Mock auth dictionary
+    """
     auth = {
       "name": "Executor",
       "reason": "reason for access",
