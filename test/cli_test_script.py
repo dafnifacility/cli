9dc71de340677e15a8b43c6a38da023c0d5cd1ac
Joel Davies <joel.davies@stfc.ac.uk>
2023-04-26 16:05:19 +0100

    Add remaining commands from script (but don't run them)

--- test/cli_test_script.py
+++ test/cli_test_script.py
@@ -1,8 +1,12 @@
 """
 Script for running tests of the cli as a whole via the command line
 
-Note: Should login just before running with --snapshot as can time out
-      and get stuck
+Notes on usage:
+    - Replace DAFNI_SCRIPT_LOCATION and SNAPSHOT_SAVE_LOCATION as necessary
+      before executing
+    - Run on python command line e.g. python ./test/cli_test_script --help
+    - Should login just before running with --snapshot as otherwise token
+      could time out and get stuck
 """
 
 from pathlib import Path
@@ -21,6 +25,7 @@ SNAPSHOT_SAVE_LOCATION = "/home/joel/dafni_cli_snapshots/"
 COMMANDS = {
     "login": ["dafni login --help", "dafni login"],
     "get": {
+        "help": ["dafni get --help"],
         "models": [
             "dafni get models --help",
             "dafni get models",
@@ -88,28 +93,89 @@ COMMANDS = {
             "dafni get workflow 4a7c1897-e902-4966-b4a8-d8c4c64ff092 797e8ba2-539d-4284-ba86-dea4a930206e --version-history --json",
         ],
     },
+    # The following commented out tests may be automated, but are tricker and
+    # can effect the others - probably best to run these manually for now
+    # anyway
+    "upload": {
+        "help": ["dafni upload --help"],
+        "model": [
+            "dafni upload model --help",
+            # 'dafni upload model model_definition.yaml tiny-example.tar --version-message "Version message"',
+            # 'dafni upload model model_definition.yaml tiny-example.tar --version-message "Child model" --parent-model e8ab1364-e673-470b-b082-7d459b523a44',
+        ],
+        "dataset": [
+            "dafni upload dataset --help",
+            # "dafni upload dataset metadata.json file_1.txt file_2.csv file_3.dat",
+        ],
+        "workflow": [
+            "dafni upload workflow --help",
+            # "dafni upload workflow workflow-upload.json",
+            # "dafni upload workflow-params workflow-upload.json",
+        ],
+    },
+    "delete": {
+        "help": ["dafni delete --help"],
+        "model": [
+            "dafni delete model --help",
+            # "dafni delete model 5ec7ebcc-7fcc-451d-8bd3-74e25ab9ccc9",
+        ],
+        "workflow": [
+            "dafni delete workflow --help",
+            # "dafni delete workflow 5ec7ebcc-7fcc-451d-8bd3-74e25ab9ccc9",
+        ],
+    },
+    "download": {
+        "help": ["dafni download --help"],
+        "dataset": [
+            # "dafni download dataset 6f6c7fb8-2f04-4ffc-b7a9-58dc2739d8c2 d8d8b3ae-9d33-42fe-bfb6-ba1d7c5f0d58 6f6c7fb8-2f04-4ffc-b7a9-58dc2739d8c2 d8d8b3ae-9d33-42fe-bfb6-ba1d7c5f0d58 --directory path\to\directory"
+        ],
+    },
+    "logout": ["dafni logout --help", "dafni logout"],
 }
 
 
 class Output:
+    """Class for storing output of running the tests"""
+
     def __init__(self):
         self.succeeded_commands = []
         self.failed_commands = []
 
     def print_output(self):
+        """Outputs a summary of the commands that succeeded/failed"""
         print(
             f"{len(self.succeeded_commands)}/{len(self.succeeded_commands) + len(self.failed_commands)} succeeded"
         )
+        if self.failed_commands:
+            print("------ Summary of failed commands -----")
+            for command in self.failed_commands:
+                print(command)
 
 
 def clean_string(string: str):
+    """Returns a clean string that could make a valid file name"""
     return "".join(e for e in string if e.isalnum())
 
 
 def save_and_check_snapshot(
     command: str, run_result: subprocess.CompletedProcess, snapshot: int, output: Output
 ):
+    """Saves the result of a command as a snapshot or otherwise checks the
+    result agains an already existing one
+
+    Args:
+        command (str): Command that was run
+        run_result (subprocess.CompletedProcess): Result of running the
+                                                  command
+        snapshot (int): Integer value specifying what to do:
+                        0 - Do nothing
+                        1 - Compare the result with a stored snapshot
+                        2 - Overwrite any stored snapshot
+        output (Output): Object for storing whether commands have succeeded
+                         or not
+    """
     if run_result.returncode != 0:
+        # Command itself failed to execute
         print(f"[FAILED!] {command}")
         # print(run_result.stderr)
         output.failed_commands.append(command)
@@ -118,6 +184,7 @@ def save_and_check_snapshot(
     # Path to compare snapshot to
     path = Path(SNAPSHOT_SAVE_LOCATION, f"{clean_string(command)}.out")
 
+    # Store as a string array with new lines ready for saving/comparing
     string_output = run_result.stdout.decode().splitlines()
     string_output = [line + "\n" for line in string_output]
 
@@ -135,12 +202,13 @@ def save_and_check_snapshot(
         with open(path, "r", encoding="utf-8") as file:
             previous_contents = file.readlines()
 
-        if previous_contents != output:
+        if previous_contents != string_output:
             print(f"[Failed] {command}")
+            print()
             print("Expected:")
-            print(previous_contents)
+            print("".join(previous_contents))
             print("Got:")
-            print(string_output)
+            print("".join(string_output))
             output.failed_commands.append(command)
             return
 
@@ -150,7 +218,20 @@ def save_and_check_snapshot(
 
 
 def run_command(command: str, snapshot: int, output: Output):
+    """Runs a given command checking it against a snapshot if necessary
+
+    Args:
+        command (str): Command to execute
+        snapshot (int): Integer value specifying what to do:
+                        0 - Do nothing
+                        1 - Compare the result with a stored snapshot
+                        2 - Overwrite any stored snapshot
+        output (Output): Object for storing whether commands have succeeded
+                         or not
+    """
     if snapshot:
+        # Run command, hiding input (this is where login may get stuck if
+        # token times out)
         run_result = subprocess.run(
             command.replace("dafni", DAFNI_SCRIPT_LOCATION),
             check=False,
@@ -159,6 +240,7 @@ def run_command(command: str, snapshot: int, output: Output):
         )
         save_and_check_snapshot(command, run_result, snapshot, output)
     else:
+        # Run command waiting for user input before running the next one
         print(f"--------- {command} ---------")
         run_result = subprocess.run(
             command.replace("dafni", DAFNI_SCRIPT_LOCATION),
@@ -181,6 +263,24 @@ def run_commands(
     output: Output,
     prefix: Optional[str] = None,
 ):
+    """Runs set of commands given as either a dictionary or a list
+
+    Args:
+        commands (dict or list): Either a dictionary containing keys
+                        representing sections with lists of commands to run
+                        or a list containing the commands. This function is
+                        recursive so additional sections in a dictionary are
+                        run (but with a different 'prefix')
+        snapshot (int): Integer value specifying what to do:
+                        0 - Do nothing
+                        1 - Compare the result with a stored snapshot
+                        2 - Overwrite any stored snapshot
+        output (Output): Object for storing whether commands have succeeded
+                         or not
+        prefix (str): Prefix for identifying the section being run in the
+                      output - typically the keys in the above commands
+                      dictionary
+    """
     # If a dict then expect subsections
     if isinstance(commands, dict):
         for key, section in commands.items():
@@ -188,6 +288,7 @@ def run_commands(
                 section, snapshot, output, f"{prefix}.{key}" if prefix else key
             )
     elif isinstance(commands, list):
+        # Run commands in the list
         print(f"--------- Testing section: {prefix} ---------")
         print()
         for command in commands:
@@ -195,7 +296,10 @@ def run_commands(
 
 
 @click.command()
-@click.option("--section", help="Section of commands to execute")
+@click.option(
+    "--section",
+    help="Section of commands to execute. For nested sections use '.' e.g. --section get.datasets",
+)
 @click.option(
     "--snapshot",
     help="Whether to snapshot the result and compare to a previous run. When true, commands will not wait and the output will not be visible.",
@@ -209,6 +313,8 @@ def run_commands(
     default=False,
 )
 def run_tests(section, snapshot, snapshot_overwrite):
+    """Executes the tests"""
+
     commands = COMMANDS
 
     # Take 1 to mean compare, 2 to mean override
