c5fd2307a0c13bb0e14caaf2350cb2199d1254b0
Joel Davies <joel.davies@stfc.ac.uk>
2023-05-19 12:49:49 +0100

    Update utils.py tests

--- dafni_cli/utils.py
+++ dafni_cli/utils.py
@@ -10,8 +10,9 @@ import click
 
 
 def prose_print(prose: str, width: int):
-    """
-    Prints string as separate paragraphs with appropriate line breaks at specified line lengths
+    """Prints string as separate paragraphs with appropriate line breaks at
+    specified line lengths
+
     Args:
         prose (str): string to print as prose
         width (int): width of the lines before a new line
@@ -21,44 +22,34 @@ def prose_print(prose: str, width: int):
             click.echo(line)
 
 
-def process_response_to_class_list(response: List[dict], class_instance: object):
-    """
-    Produces a list of objects of a specified class from a list of dictionaries obtained from the DAFNI API.
-
-    Args:
-        response (list[dict]): List of dictionaries returned from the DAFNI API.
-        class_instance (object): Class to create a list of.
-
-    Returns:
-        class_list (list[object]): List of objects with the attributes populated from the dictionaries.
-    """
-    class_list = []
-    for class_dict in response:
-        instance = class_instance()
-        instance.set_attributes_from_dict(class_dict)
-        class_list.append(instance)
-    return class_list
-
-
 def optional_column(value: Optional[Any], column_width: int = 0, alignment: str = "<"):
-    """Adds a value to a column, if the key exists in the dictionary
-    and adds spaces of the appropriate width if not.
+    """Formats a value that may be None to have a specific width in a column
+
+    When the value is None, will return a string with spaces of the desired
+    width
 
     Args:
-         value (Optional[Any]): Data that is to be checked and added if present
-         column_width (int): Number of spaces to be returned instead if the key is not present
+         value (Optional[Any]): Data that is to be checked and formatted if
+                                not None
+         column_width (int): Number of spaces to be returned instead if the
+                             key is not present
          alignment (str): Specified alignment of column
     Returns:
-        entry (str): Either the value of the entry to be put into the table, column_width number of spaces
+        entry (str): Either the value of the entry to be put into the table,
+                     column_width number of spaces
+
+    Raises:
+        ValueError - If the column_width is negative
     """
+    if column_width < 0:
+        raise ValueError("Column width for optional column must be non-negative")
+
     if value is not None:
         entry_string = str(value)
         if column_width > 0:
             entry = f"{entry_string:{alignment}{column_width}}"
         elif column_width == 0:
             entry = entry_string
-        else:
-            raise ValueError("Column width for optional column must be non-negative")
     else:
         entry = " " * column_width
     return entry
