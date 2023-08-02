2278166aeea47ef571bdc1a8f8917df9816be723
Joel Davies <joel.davies@stfc.ac.uk>
2023-08-02 11:03:34 +0100

    Update upload_file_to_minio unit test

--- test/api/test_minio_api.py
+++ test/api/test_minio_api.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from unittest import TestCase
-from unittest.mock import MagicMock, mock_open, patch
+from unittest.mock import ANY, MagicMock, mock_open, patch
 
 from dafni_cli.api import minio_api
 from dafni_cli.consts import (
@@ -16,23 +16,60 @@ class TestMinioAPI(TestCase):
     """Test class to test the functions in minio_api.py"""
 
     @patch("builtins.open", new_callable=mock_open, read_data="definition file")
-    def test_upload_file_to_minio(self, open_mock):
+    @patch("dafni_cli.api.minio_api.create_file_progress_bar")
+    @patch("dafni_cli.api.minio_api.CallbackIOWrapper")
+    def test_upload_file_to_minio(
+        self, mock_CallbackIOWrapper, mock_create_file_progress_bar, open_mock
+    ):
         """Tests that upload_file_to_minio works as expected"""
 
         # SETUP
         session = MagicMock()
         url = "example.url"
-        file_path = Path("path/to/file")
+        file_path = MagicMock(name="file_name", stat=lambda: MagicMock(st_size=1000))
+        progress_bar = MagicMock()
+        mock_progress_bar = MagicMock()
+
+        # Cause the refresh callback to be called
+        put_request_return_value = MagicMock()
+
+        def put_request_side_effect(url, content_type, data, refresh_callback):
+            refresh_callback()
+            return put_request_return_value
+
+        session.put_request = MagicMock(side_effect=put_request_side_effect)
+
+        mock_create_file_progress_bar.return_value.__enter__.return_value = (
+            mock_progress_bar
+        )
 
         # CALL
-        result = minio_api.upload_file_to_minio(session, url, file_path)
+        result = minio_api.upload_file_to_minio(
+            session, url, file_path, progress_bar=progress_bar
+        )
 
         # ASSERT
         open_mock.assert_called_once_with(file_path, "rb")
-        session.put_request.assert_called_once_with(
-            url=url, content_type=MINIO_UPLOAD_CT, data=open(Path(file_path), "rb")
+        mock_create_file_progress_bar.assert_called_once_with(
+            desc=file_path.name,
+            total=file_path.stat().st_size,
+            disable=False,
         )
-        self.assertEqual(result, session.put_request.return_value)
+        mock_CallbackIOWrapper.assert_called_once_with(
+            mock_progress_bar.update, open_mock.return_value, "read"
+        )
+        session.put_request.assert_called_once_with(
+            url=url,
+            content_type=MINIO_UPLOAD_CT,
+            data=mock_CallbackIOWrapper.return_value,
+            refresh_callback=ANY,
+        )
+
+        # refresh_callback
+        open_mock.return_value.seek.assert_called_once_with(0)
+        mock_progress_bar.reset.assert_called_once()
+
+        self.assertEqual(result, put_request_return_value)
 
     def test_create_temp_bucket(self):
         """Tests that create_temp_bucket works as expected"""
