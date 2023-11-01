5e0fa97b45abb96295a72bda53535119e217b4a3
Sarah <69518420+sarahbyrnie@users.noreply.github.com>
2023-11-01 16:52:21 +0000

    fix: extends shutdown notification
    * Extends shutdown notification
    
    * tests

--- dafni_cli/api/notifications_api.py
+++ dafni_cli/api/notifications_api.py
@@ -7,7 +7,7 @@ def get_notifications() -> List[str]:
     notifications = [
         {
             "start_date": datetime(2023, 10, 15),
-            "end_date": datetime(2023, 11, 1),
+            "end_date": datetime(2023, 11, 4),
             "message": "DAFNI will be shutting down for mandatory electrical safety checks and as a result will be unavailable from 27th to 31st October. DAFNI will not be accepting any new Data, Models or Workflows from 25th October as we cannot guarantee any assets submitted after this date will finish ingesting/running before the shutdown. Please see https://www.dafni.ac.uk/important-notice-to-dafni-users/ for more information.",
         }
     ]
